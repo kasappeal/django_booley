5129d9c7c4ae53582727bf49d291ac56718613b3
Alberto Casero <alberto@kasfactory.net>
2016-12-14 10:37:57 +0100

    Installation issues fixed

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name="django_booley",
-    version="0.1.0",
+    version="0.1.1",
     author="Alberto Casero",
     author_email="kas.appeal@gmail.com",
-    include_package_data=True,
-    url="http://pypi.python.org/pypi/django_booley/",
-    license="LICENSE.txt",
+    packages=["django_booley"],
+    url="https://github.com/kasappeal/django_booley",
+    license="MIT",
     description="Booley integration for Django with BooleyField for models.",
     install_requires=["booley", "Django"],
 )
